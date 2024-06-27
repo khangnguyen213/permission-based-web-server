4947e3dd02646666d3d174079d22d4b7a8e0b187
khangnguyen213 <khangnguyenm01@gmail.com>
2024-06-27 11:03:59 +0700

    add test but still got error

--- backend_rep/src/modules/user/user.service.spec.ts
+++ backend_rep/src/modules/user/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('UserService', () => {
   let service: UserService;
