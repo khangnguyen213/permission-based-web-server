a732063173fb42b1262712755235aeb0f11c7914
khangnguyen213 <khangnguyenm01@gmail.com>
2024-07-01 01:01:56 +0700

    add user and role page

--- frontend_rep/src/app/(authenticated)/dashboard/page.tsx
+++ frontend_rep/src/app/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard</p>
+      <ul>
+        <Button>
+          <Link href="user">Users</Link>
+        </Button>
+        <Button>
+          <Link href="role">Roles</Link>
+        </Button>
+      </ul>
+    </div>
+  );
 }
 
 export default DashboardPage;
---
+++ frontend_rep/src/app/(authenticated)/user/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserTable from './components/user-table';
+
+function User() {
+  return (
+    <div>
+      <h1>User Page</h1>
+      <UserTable />
+    </div>
+  );
+}
+
+export default User;
