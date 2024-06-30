26da04d4a2fe6f828923d06cb4b6baefe535ebf5
khangnguyen213 <khangnguyenm01@gmail.com>
2024-07-01 01:25:37 +0700

    chore: Update authenticated layout to display user information

--- frontend_rep/src/app/(authenticated)/layout.tsx
+++ frontend_rep/src/app/(authenticated)/layout.tsx
@@ -17,13 +17,27 @@ export default function AuthenticatedLayout({
   }, [sessionStore.data, sessionStore.loading]);
   return (
     <main>
-      <div>Role: {sessionStore.data?.role?.name || 'Not assigned'}</div>
-      <div>Your permissions:</div>
-      <ul>
-        {sessionStore.data?.role?.permissions.map((permission) => (
-          <li key={permission.name}>{permission.name}</li>
-        ))}
-      </ul>
+      <div className="p-2 w-fit mx-auto border border-slate-100 rounded-sm">
+        <div>
+          <span className="p-1 bg-slate-500 rounded-sm">ID</span>{' '}
+          {sessionStore.data?.id || ''}
+        </div>
+        <div>
+          <span className="p-1 bg-slate-500 rounded-sm">Email</span>{' '}
+          {sessionStore.data?.email || ''}
+        </div>
+        <div>
+          <span className="p-1 bg-slate-500 rounded-sm">Role</span>{' '}
+          {sessionStore.data?.role?.name || 'Not assigned'}
+        </div>
+        <div>
+          <span className="p-1 bg-slate-500 rounded-sm">Permissions</span>{' '}
+          {sessionStore.data?.role?.permissions
+            .map((permission) => permission.name)
+            .join(', ') || 'Not assigned'}
+        </div>
+      </div>
+
       {sessionStore.data && children}
     </main>
   );
