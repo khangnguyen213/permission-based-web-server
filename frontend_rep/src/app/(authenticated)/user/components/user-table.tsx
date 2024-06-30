66e9f7b5e60f6015db2ff5f7a636b71992952192
khangnguyen213 <khangnguyenm01@gmail.com>
2024-07-01 01:02:21 +0700

    add user table

---
+++ frontend_rep/src/app/(authenticated)/user/components/user-table.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { UserDto } from '@/common/interfaces';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { userApi } from '@/service/userApi';
+import { useEffect, useState } from 'react';
+
+function UserTable() {
+  const [users, setUsers] = useState<UserDto[]>([]);
+
+  useEffect(() => {
+    userApi.getAll().then((res) => {
+      if (res.err) {
+        console.log(res.err);
+        return;
+      }
+      if (res.data) {
+        setUsers(res.data);
+      }
+    });
+  }, []);
+  return (
+    <div className="w-[80%] mx-auto">
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>#</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>createdAt</TableHead>
+            <TableHead>updatedAt</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map((user, i) => (
+            <TableRow key={user.id}>
+              <TableCell>{i + 1}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.roleId}</TableCell>
+              <TableCell>{user.createdAt.toLocaleString()}</TableCell>
+              <TableCell>{user.updatedAt.toLocaleString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export default UserTable;
