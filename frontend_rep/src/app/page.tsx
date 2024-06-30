c04946b2b0b75cde67caed570b3bb395800d8cff
khangnguyen213 <khangnguyenm01@gmail.com>
2024-07-01 01:01:34 +0700

    delete unecessary test

--- frontend_rep/src/app/page.tsx
+++ frontend_rep/src/app/page.tsx
@@ -1,25 +1,7 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
 function Home() {
-  const [permissions, setPermissions] = useState<{ name: string }[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/permission')
-      .then((res) => res.json())
-      .then((data) => {
-        setPermissions(data.data);
-      });
-  }, []);
   return (
     <div>
       <h1>Home</h1>
-      <ul>
-        {permissions.map((permission) => (
-          <li key={permission.name}>{permission.name}</li>
-        ))}
-      </ul>
     </div>
   );
 }
