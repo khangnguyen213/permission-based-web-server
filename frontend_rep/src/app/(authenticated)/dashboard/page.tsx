ab314343038777a49f16966b8074ed1cb9fce842
khangnguyen213 <khangnguyenm01@gmail.com>
2024-07-02 11:49:22 +0700

    refactor: Update page content

--- frontend_rep/src/app/(authenticated)/dashboard/page.tsx
+++ frontend_rep/src/app/(authenticated)/dashboard/page.tsx
@@ -3,17 +3,20 @@ import Link from 'next/link';
 
 function DashboardPage() {
   return (
-    <div className="w-[80%] mx-auto">
+    <div className="w-[80%] mx-auto my-32">
       <h1 className="text-center text-2xl p-8">Dashboard</h1>
       <p className="text-center">Welcome to the dashboard</p>
       <p className="text-center">Only authenticated user can see this page</p>
-      <ul className="flex gap-1 w-fit mx-auto">
-        <Link href="user">
-          <Button>Users</Button>
-        </Link>
-        <Link href="role">
-          <Button>Roles</Button>
-        </Link>
+      <ul className="flex flex-col items-center gap-3 w-fit mx-auto my-8">
+        <p>Only account with specific permissions can access these page</p>
+        <div className="flex gap-3">
+          <Link href="user">
+            <Button>Users</Button>
+          </Link>
+          <Link href="role">
+            <Button>Roles</Button>
+          </Link>
+        </div>
       </ul>
     </div>
   );
--- frontend_rep/src/app/page.tsx
+++ frontend_rep/src/app/page.tsx
@@ -1,8 +1,25 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 function Home() {
   return (
-    <div className="w-[80%] mx-auto my-8">
+    <div className="w-[80%] mx-auto my-32">
       <h1 className="text-center">Home</h1>
       <p className="text-center">This page is public for everyone</p>
+      <div className="w-fit items-center mx-auto my-8 flex gap-3">
+        <Link href="/dashboard">
+          <Button>Dashboard</Button>
+        </Link>
+        (you can not access dashboard page unless you logged in)
+      </div>
+      <div className="w-fit items-center mx-auto my-8 flex gap-3">
+        <Link href="/login">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/register">
+          <Button>Register</Button>
+        </Link>
+      </div>
     </div>
   );
 }
